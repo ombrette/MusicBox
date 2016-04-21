22d09d56aba71fde5a71c74ad8d3ff5bf53ec01c
Joan Ortega <jomaora@gmail.com>
2016-04-21 23:15:44 +0200

    Voici mes remarques

--- routes/index.js
+++ routes/index.js
@@ -8,9 +8,12 @@ var TopService = require('../services/top');
 router.get('/', function(req, res, next) {
   if (req.accepts('text/html') || req.accepts('application/json')) {
         TopService.getTop5SongsByNotes(req.query || {})
+            // je comprends pas pourquoi on a besoin d'une query ici000
             .then(function(songs) {
+
                 if (req.accepts('text/html')) {
                     return res.render('index', {top: songs});
+                    // c'est dommage, tu passes bien l'objet mais par contre tu l'exploites pas sur la vue
                 }
                 if (req.accepts('application/json')) {
                     res.status(200).send(songs);
